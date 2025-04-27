5b4ac07d9651997c9e4636d8fe92af93a6fb8d25
Raina <raina@kaleidoscope.com>
2025-04-27 14:39:14 +0100

    add tmux config

--- shell/tmux.nix
+++ shell/tmux.nix
@@ -1,13 +1,32 @@
 {pkgs, ...}: {
-  programs.tmux.enable = true;
-  programs.tmux.prefix = "C-a";
-  programs.tmux.plugins = with pkgs.tmuxPlugins; [
-    {
-	plugin = gruvbox;
-	extraConfig = ''
-	  run ${gruvbox}/tmux-gruvbox.nix
-	  set -g @tmux-gruvbox 'dark'
-	'';
-    }
-  ]; 
+  programs.tmux = {
+    enable = true;
+    prefix = "C-a";
+    plugins = with pkgs.tmuxPlugins; [
+      {
+          plugin = gruvbox;
+          extraConfig = ''
+            run ${gruvbox}/tmux-gruvbox.nix
+            set -g @tmux-gruvbox 'dark'
+          '';
+      }
+    ]; 
+
+    extraConfig = ''
+      set -g default-terminal "xterm-256color"
+      set -ag terminal-overrides ",xterm-256color:RGB:Sxl"
+
+      set -s extended-keys always
+      set -as terminal-features 'xterm-kitty*:extkeys'
+
+      set -gq allow-passthrough on
+
+      bind -n M-x split-window -v -c "#{pane_current_path}"
+      bind -n M-v split-window -h -c "#{pane_current_path}"
+      bind c new-window -c "#{pane_current_path}"
+
+      set-option -g automatic-rename-format '#{b:pane_current_path}'
+    '';
+  };
+
 }
